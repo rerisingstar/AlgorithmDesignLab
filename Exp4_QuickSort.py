8b2ca001dbbd926cbf5baa89a0f48991fa8b902e
Workpc <junzhewjz@gmail.com>
2024-04-26 01:09:19 +0800

    exp4 stack overflow

--- Exp4_QuickSort.py
+++ Exp4_QuickSort.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pickle
 import random
+import time
+import sys  # 导入sys模块
+sys.setrecursionlimit(3000)
 
 def generate_lists(num_lists=11):
     lists = []
@@ -20,9 +23,10 @@ def generate_lists(num_lists=11):
     return lists
 
 def exchange(A, i, j):
-    tmp = A[i]
-    A[i] = A[j]
-    A[j] = tmp
+    if i != j:
+        tmp = A[i]
+        A[i] = A[j]
+        A[j] = tmp
 
 def Rand_Partition(A, p, r):
     i = random.randint(p, r)
@@ -44,6 +48,26 @@ def QuickSort(A, p, r):
         QuickSort(A, q+1, r)
     return A
 
+def quicksort(A, low, high):
+
+    def partition(A, low, high):
+
+        pivot = A[high]
+        i = low - 1  
+        for j in range(low, high):
+            if A[j] <= pivot:
+                i = i + 1
+                A[i], A[j] = A[j], A[i]
+        A[i + 1], A[high] = A[high], A[i + 1]
+        return i + 1
+
+    if low < high:
+
+        pi = partition(A, low, high)
+
+        quicksort(A, low, pi - 1)
+        quicksort(A, pi + 1, high)
+
 if __name__ == "__main__":
     data_path = "./data/Exp3/lists.pkl"
     # lists = generate_lists()
@@ -53,8 +77,17 @@ if __name__ == "__main__":
         lists = pickle.load(f)
     
     # data1 = lists[0]
-    data1 = np.array(list(range(20)))
-    np.random.shuffle(data1)
-    data_sort = QuickSort(data1, 0, len(data1)-1)
-    print(data_sort)
+    # data1 = np.array(list(range(5)) + [3, 3, 4, 4, 5])
+    # np.random.shuffle(data1)
+    # data_sort = QuickSort(data1, 0, len(data1)-1)
+    # print(data_sort)
     
+    for i in range(2, 11):
+        data = lists[i]
+        print(f"{i}th data, Repeat number = {10**4 * 10 * i}")
+        start_time = time.perf_counter()
+        data_sort = quicksort(list(data), 0, len(data)-1)
+        
+        end_time = time.perf_counter()
+        elapsed_time = end_time - start_time
+        print(f"    Algorithm execution time: {elapsed_time:.3f} s")
